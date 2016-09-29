67d0d004907c2fbd335c1141ce7096a5e7c0bad0
Александр <bigcoster@gmail.com>
2016-09-30 02:01:13 +0300

    Сделал файл настроек, если отсутствует, то создастся новый с дефолтными значениями.

--- main4.py
+++ main4.py
@@ -4,29 +4,36 @@ from influxdb import InfluxDBClient
 from influxdb import SeriesHelper
 from datetime import datetime
 from serial import Serial
+from os import path
 import logging
+from configparser import ConfigParser
 
-
-logging.basicConfig(filename='NTC100 '+datetime.now().strftime("%H%M%S")+'.log', level=logging.WARNING, format='%(asctime)s %(message)s',
+logging.basicConfig(filename='NTC100 '+datetime.now().strftime("%H%M%S")+'.log',
+                    level=logging.WARNING, format='%(asctime)s %(message)s',
                     datefmt='%m.%d.%Y %H:%M:%S')
+config = ConfigParser()
+# def values:
+config['influxdb'] = {'host': 'localhost', 'port': '8086', 'user': 'root',
+                      'pass': 'root', 'db': 'mydb','retention_days':'30'}
+config['comport'] = {'name': 'COM1', 'boudrate': '57600'}
+config['logging'] = {'enabled':'True'}
 
-# InfluxDB connections settings
-host = 'localhost'
-port = 8086
-user = 'root'
-password = 'root'
-dbname = 'mydb'
-# comport = 'COM1'
-comport = 'COM4'
-# comport = 'COM2'
-myclient = InfluxDBClient(host, port, user, password, dbname)
+def timenow():
+    return datetime.now().strftime('%m.%d.%Y %H:%M:%S')
 
-# myclient.drop_database(dbname)
-# Uncomment the following code if the database is not yet created
-# myclient.create_database(dbname)
-#myclient.create_retention_policy('awesome_policy', '31d', 3, default=True)
+if not path.exists('config.ini'):
+    print(timenow(), 'No config! It will be created with def values...')
+    logging.warning('No config! It will be created with def values...')
+    with open('config.ini', 'w') as configfile:
+        config.write(configfile)
+else:
+    config.read('config.ini')
+
+myclient = InfluxDBClient(config['influxdb']['host'], config['influxdb']['port'],
+                          config['influxdb']['user'], config['influxdb']['pass'],
+                          config['influxdb']['db'])
+
 tmp_data = {}
-
 
 class MySeriesHelper(SeriesHelper):
     # Meta class stores time series helper configuration.
@@ -37,7 +44,7 @@ class MySeriesHelper(SeriesHelper):
         # series_name = 'events.stats.{server_name}'
         series_name = 'NTC100'
         # Defines all the fields in this time series.
-        fields = ['curr_temp', 'task_temp', 'curr_work', 'frame', 'time_resp','dev_resp']
+        fields = ['curr_temp', 'task_temp', 'curr_work', 'frame', 'time_resp', 'dev_resp']
         # Defines all the tags for the series.
         tags = ['dev_addr']
         # Defines the number of data points to store prior to writing on the wire.
@@ -45,58 +52,45 @@ class MySeriesHelper(SeriesHelper):
         # autocommit must be set to True when using bulk_size
         autocommit = True
 
-
-with Serial(comport, 57600, timeout=15) as ser:
-# with Serial(comport, 9600, timeout=1) as ser:
-    while True:    
-        # ser.write(b'a')
-        # time.sleep(0.5)
+try:
+    ser = Serial(config['comport']['name'], config['comport']['boudrate'], timeout=15)
+    while True:
         inbytes = ser.readline()
-
-        # logging.warning("get data: {}".format(inbytes))
-
         if inbytes:
-            recived_data = inbytes
             try:
                 inbytes = inbytes.decode("ascii")
-                print(datetime.now(),inbytes.strip())
+                print(timenow(), inbytes.strip())
                 logging.warning(inbytes.strip())
                 try:
                     data = json.loads(inbytes)
-                    # print(json.dumps(data, sort_keys=True, indent=None))
                     if data != tmp_data:
                         tmp_data = data
-                        # print("Storred data:")
                         for dev in sorted(data):
-                            # print("{0}:[{1},{2},{3},{4}]".format(dev, data[dev][0], data[dev][1], data[dev][2]))
                             MySeriesHelper(dev_addr=dev, curr_temp=data[dev][0], task_temp=data[dev][1],
-                                           curr_work=data[dev][2], frame=data[dev][3],time_resp=data[dev][4],
+                                           curr_work=data[dev][2], frame=data[dev][3], time_resp=data[dev][4],
                                            dev_resp=data[dev][5])
-
-                        # To inspect the JSON which will be written, call _json_body_():
-                        # print(MySeriesHelper._json_body_())
-
-                        # To manually submit data points which are not yet written, call commit:
                         try:
                             MySeriesHelper.commit()
                         except Exception as msg:
-                            print(msg)
-                    # print("Read ast_dataFrame")
-                    # data = myclient.query("select * from NTC100")
-                    # print(data)
+                            print(timenow(), msg)
+                            logging.error(msg)
                     else: 
-                       print ('duplicated data')
+                        print (timenow(),'^ Dublicated data, not write in db')
+                        logging.warning('^ Dublicated data, not write in db')
                 except Exception as msg:
-                    print('Corrupted json')
-                    print(msg)
-                    logging.warning('Corrupted json')
+                    print(timenow(),'^ Corrupted json')
+                    print(timenow(), msg)
+                    logging.warning('^ Corrupted json')
                     logging.warning(msg)
             except Exception as msg:
-                print(datetime.now(),recived_data,' <- Corrupted ascii.')
-                print(msg)
-                logging.warning(recived_data, ' <- Corrupted ascii.')
+                print(timenow(),'^ Corrupted ascii')
+                print(timenow(),msg)
+                logging.warning('^ Corrupted ascii')
                 logging.warning(msg)
-        # else:
-        #     print('Empty data.')
         time.sleep(1)
+except Exception as msg:
+    print(timenow(), 'Can`t open ', config['comport']['name'])
+    print(timenow(), msg)
+    logging.warning('Can`t open ', config['comport']['name'])
+    logging.warning(msg)
 quit()
