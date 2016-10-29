521e7f2136d0a5b7e74623d4a6c2e9d9e1d0c1bf
Александр <bigcoster@gmail.com>
2016-10-29 03:22:13 +0300

    Дороботал логирование. Теперь есть ротация лога и загрузка уровня логирования из конф файла.

--- main4.py
+++ main4.py
@@ -6,31 +6,44 @@ from datetime import datetime
 from serial import Serial
 from os import path
 import logging
+import logging.handlers
 from configparser import ConfigParser
 
-def timenow():
-    return datetime.now().strftime('%m.%d.%Y %H:%M:%S')
+#conf logging
+levels = {'debug':logging.DEBUG, 'info':logging.INFO, 'warning':logging.WARNING,
+          'error':logging.ERROR, 'critical':logging.CRITICAL}
+log = logging.getLogger(__name__)
+log.setLevel(logging.INFO)
+formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s', datefmt='%m.%d.%Y %H:%M:%S')
+console = logging.StreamHandler()
+console.setFormatter(formatter)
+# filehandler = logging.FileHandler('NTC1001.log')
+filehandler = logging.handlers.RotatingFileHandler('NTC1001.log', maxBytes=1024000, backupCount=5)
+# filehandler = logging.handlers.TimedRotatingFileHandler('NTC1001.log', when='M', backupCount=7)
 
-logging.basicConfig(filename='NTC100.log', level=logging.WARNING,
-                    format='%(asctime)s %(message)s',datefmt='%m.%d.%Y %H:%M:%S')
-print(timenow(), 'Start application')
-logging.warning('Start application')
+filehandler.setFormatter(formatter)
+log.addHandler(console)
+log.addHandler(filehandler)
+
+log.info('Start application')
+
+
 config = ConfigParser()
 # def values:
 config['influxdb'] = {'host': 'localhost', 'port': '8086', 'user': 'root',
                       'pass': 'root', 'db': 'mydb','retention_days':'30'}
 config['comport'] = {'name': 'COM1', 'boudrate': '57600'}
-config['logging'] = {'enabled':'True'}
-
-
+config['logging'] = {'level':'info'}
 
 if not path.exists('config.ini'):
-    print(timenow(), 'No config! It will be created with def values...')
-    logging.warning('No config! It will be created with def values...')
+    log.warning('No config! It will be created with def values...')
     with open('config.ini', 'w') as configfile:
         config.write(configfile)
 else:
     config.read('config.ini')
+log.info('Logging level: ' + config['logging']['level'].upper())
+log.setLevel(levels[config['logging']['level']])
+
 
 myclient = InfluxDBClient(config['influxdb']['host'], config['influxdb']['port'],
                           config['influxdb']['user'], config['influxdb']['pass'],
@@ -59,43 +72,35 @@ class MySeriesHelper(SeriesHelper):
 try:
     ser = Serial(config['comport']['name'], config['comport']['boudrate'], timeout=15)
     while True:
-        inbytes = ser.readline()
+        comm_data = inbytes = ser.readline()
         if inbytes:
             try:
                 inbytes = inbytes.decode("ascii")
-                print(timenow(), inbytes.strip())
-                logging.warning(inbytes.strip())
+                log.info(inbytes.strip())
                 if not (inbytes == start_msg): 
-	                try:
-	                    data = json.loads(inbytes)
-	                    if data != tmp_data:
-	                        tmp_data = data
-	                        for dev in sorted(data):
-	                            MySeriesHelper(dev_addr=dev, curr_temp=data[dev][0], task_temp=data[dev][1],
+                    try:
+                        data = json.loads(inbytes.strip())
+                        if data != tmp_data:
+                            tmp_data = data
+                            for dev in sorted(data):
+                                MySeriesHelper(dev_addr=dev, curr_temp=data[dev][0], task_temp=data[dev][1],
 	                                           curr_work=data[dev][2], frame=data[dev][3], time_resp=data[dev][4],
 	                                           dev_resp=data[dev][5])
-	                        try:
-	                            MySeriesHelper.commit()
-	                        except Exception as msg:
-	                            print(timenow(), msg)
-	                            logging.error(msg)
-	                    else: 
-	                        print (timenow(),'^ Dublicated data, not write in db')
-	                        logging.warning('^ Dublicated data, not write in db')
-	                except Exception as msg:
-	                    print(timenow(),'^ Corrupted json')
-	                    print(timenow(), msg)
-	                    logging.warning('^ Corrupted json')
-	                    logging.warning(msg)
+                            try:
+                                MySeriesHelper.commit()
+                            except Exception as msg:
+                                log.error('Coul`d not connect to influxdb')
+                                log.error(msg)
+                        else:
+                            log.warning('^ Dublicated data, not write in db')
+                    except Exception as msg:
+                        log.warning('Corrupted json -> ' + inbytes.strip())
+                        log.warning(msg)
             except Exception as msg:
-                print(timenow(),'^ Corrupted ascii')
-                print(timenow(),msg)
-                logging.warning('^ Corrupted ascii')
-                logging.warning(msg)
+                log.warning('Corrupted ascii -> ' + comm_data)
+                log.warning(msg)
         time.sleep(1)
 except Exception as msg:
-    print(timenow(), 'Can`t open ', config['comport']['name'])
-    print(timenow(), msg)
-    logging.warning('Can`t open ', config['comport']['name'])
-    logging.warning(msg)
+    log.error('Can`t open ' + config['comport']['name'])
+    log.error(msg)
 quit()
